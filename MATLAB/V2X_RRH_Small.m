3eece7e983d9a21ca38a4c81c8da6b8563e670ab
Faysal-Marzuk <80412760+Faysal-Marzuk@users.noreply.github.com>
2023-02-15 09:13:17 +0100

    Update V2X_RRH_Small.m

--- MATLAB/V2X_RRH_Small.m
+++ MATLAB/V2X_RRH_Small.m
@@ -1,4 +1,5 @@
 clear; clc; clf;
+
 x1 = 0; x2 = 2;
 y1 = 0; y2 = 2;
 x = [x1, x2, x1, x2, x1];
@@ -6,11 +7,10 @@ y = [y2, y2, y1, y1, y2];
 plot(x, y, 'b-');
 hold on;
 hConnections = [];
-numberOfCars = 50;
+numberOfVehicles = 50; % 40; 30; 20; 10;
 numberOfRRHs = 4;
-
-x3 = rand(1, numberOfCars)*2;
-y3 = rand(1, numberOfCars)*2;
+x3 = rand(1, numberOfVehicles)*2;
+y3 = rand(1, numberOfVehicles)*2;
 close all;
 figure;
 hold on;
@@ -19,10 +19,11 @@ title('Snapshots of Vehicles Positions')
 xlabel('Distance [km]')
 ylabel('Distance [km]')
 legend({'RRH','Vehicle'})
+
 for i = 1 : numberOfRRHs
     text(x(i)+0.02,y(i)+0.02, num2str(i),'Color','r')
 end
-for j = 1 : numberOfCars
+for j = 1 : numberOfVehicles
     text(x3(j)+0.01, y3(j)+0.01, num2str(j),'Color','b')
 end
 for i = 1 : size(x,1)-1
@@ -30,6 +31,7 @@ for i = 1 : size(x,1)-1
         hConnections = [hConnections ; line([x(i);x(j)], [y(i);y(j)])];
     end
 end
+
 alpha = 3.5;
 PtdBm = 23;
 Pt = 10^(PtdBm/10);
@@ -39,63 +41,97 @@ BW = 10*10^(6);
 NOcdBm = -174 + 10*log10(BW);
 NOc = 10^(NOcdBm/10);
 Distance = zeros;
+%DistMatrix = zeros;
 SINR = zeros;
 SINRdB =zeros;
+%SINRdBMatrix =zeros;
 Efficiency = zeros;
+%EfficiencyMatrix = zeros;
 DataRate = zeros;
+%DataRateMatrix = zeros;
+UpLinkDataRate = zeros;
+MaxRB = zeros;
+ReqRB = zeros;
+CeilofReqRB = zeros;
+%CeilofReqRBMatrix = zeros;
 count = 1;
 for i = 1 : numberOfRRHs
-    for j = 1 : numberOfCars
-    Distance(i,j) = sqrt((x(i) - x3(j))^2 + (y(i) - y3(j))^2);
-    DistMatrix(count, :) = [x(i) y(i) x3(j) y3(j) Distance(i,j)];
-    SINR(i,j) = (Pt*((Distance(i,j)).^-alpha))/((numberOfRRHs)*In + NOc);
+    for j = 1 : numberOfVehicles
+    Distance(i,j) = sqrt((x1(i) - x2(j))^2 + (y1(i) - y2(j))^2);
+%     DistMatrix(count, :) = [x1(i) y1(i) x2(j) y2(j) Distance(i,j)];
+    SINR(i,j) = (Pt*((Distance(i,j)).^-alpha))/((numberOfRRHs)*In + NOc); 
     SINRdB(i,j)=10*log10(SINR(i,j));
-    SINRdBMatrix(count, :) = [x(i) y(i) x3(j) y3(j) SINRdB(i,j)];
-    if (SINRdB(i,j) > -9.5) & (SINRdB(i,j) <= -6.7)
+%     SINRdBMatrix(count, :) = [x1(i) y1(i) x2(j) y2(j) SINRdB(i,j)];
+    if (SINRdB(i,j) <= -9.5)
+        Efficiency(i,j) = 0.000001;
+    elseif (SINRdB(i,j) > -9.5) && (SINRdB(i,j) <= -6.7)
         Efficiency(i,j) = 0.1523;
-    elseif (SINRdB(i,j) > -6.7) & (SINRdB(i,j) <= -4.1)
+    elseif (SINRdB(i,j) > -6.7) && (SINRdB(i,j) <= -4.1)
         Efficiency(i,j) = 0.2344;
-    elseif (SINRdB(i,j) > -4.1) & (SINRdB(i,j) <= -1.8)
+    elseif (SINRdB(i,j) > -4.1) && (SINRdB(i,j) <= -1.8)
         Efficiency(i,j) = 0.3770;
-    elseif (SINRdB(i,j) > -1.8) & (SINRdB(i,j) <= 0.4)
+    elseif (SINRdB(i,j) > -1.8) && (SINRdB(i,j) <= 0.4)
         Efficiency(i,j) = 0.6016;    
-    elseif (SINRdB(i,j) > 0.4) & (SINRdB(i,j) <= 2.5)
+    elseif (SINRdB(i,j) > 0.4) && (SINRdB(i,j) <= 2.5)
         Efficiency(i,j) = 0.8770;
-    elseif (SINRdB(i,j) > 2.5) & (SINRdB(i,j) <= 4.5)
+    elseif (SINRdB(i,j) > 2.5) && (SINRdB(i,j) <= 4.5)
         Efficiency(i,j) = 1.1758;    
-    elseif (SINRdB(i,j) > 4.5) & (SINRdB(i,j) <= 6.5)
+    elseif (SINRdB(i,j) > 4.5) && (SINRdB(i,j) <= 6.5)
         Efficiency(i,j) = 1.4766;    
-    elseif (SINRdB(i,j) > 6.5) & (SINRdB(i,j) <= 8.5)
+    elseif (SINRdB(i,j) > 6.5) && (SINRdB(i,j) <= 8.5)
         Efficiency(i,j) = 1.9141;
-    elseif (SINRdB(i,j) > 8.5) & (SINRdB(i,j) <= 10.3)
+    elseif (SINRdB(i,j) > 8.5) && (SINRdB(i,j) <= 10.3)
         Efficiency(i,j) = 2.4063;        
-    elseif (SINRdB(i,j) > 10.3) & (SINRdB(i,j) <= 12.3)
+    elseif (SINRdB(i,j) > 10.3) && (SINRdB(i,j) <= 12.3)
         Efficiency(i,j) = 2.7305;    
-    elseif (SINRdB(i,j) > 12.3) & (SINRdB(i,j) <= 14.2)
+    elseif (SINRdB(i,j) > 12.3) && (SINRdB(i,j) <= 14.2)
         Efficiency(i,j) = 3.3223;    
-    elseif (SINRdB(i,j) > 14.2) & (SINRdB(i,j) <= 15.9)
+    elseif (SINRdB(i,j) > 14.2) && (SINRdB(i,j) <= 15.9)
         Efficiency(i,j) = 3.9023;
-    elseif (SINRdB(i,j) > 15.9) & (SINRdB(i,j) <= 17.8)
+    elseif (SINRdB(i,j) > 15.9) && (SINRdB(i,j) <= 17.8)
         Efficiency(i,j) = 4.5234;    
-    elseif (SINRdB(i,j) > 17.8) & (SINRdB(i,j) <= 19.8)
+    elseif (SINRdB(i,j) > 17.8) && (SINRdB(i,j) <= 19.8)
         Efficiency(i,j) = 5.1152;    
     elseif (SINRdB(i,j) > 19.8) 
         Efficiency(i,j) = 5.5547;
     end
-       
-    EfficiencyMatrix(count, :) = [x(i) y(i) x3(j) y3(j) Efficiency(i,j)];    
-    DataRate(i,j) = 12*7*Efficiency(i,j)*1000/0.5;
-    DataRateMatrix(count, :) = [x(i) y(i) x3(j) y3(j) DataRate(i,j)];
+    count = count + 1; 
+    end
+end
+
+for i = 1 : numberOfRRHs
+    for j = 1 : numberOfVehicles
+    
+%     EfficiencyMatrix(count, :) = [x1(i) y1(i) x2(j) y2(j) Efficiency(i,j)];        
+      DataRate(i,j) = 12*7*Efficiency(i,j)*1000;   
+%     DataRateMatrix(count, :) = [x1(i) y1(i) x2(j) y2(j) DataRate(i,j)];   
+      UpLinkDataRate(j) = 1000000;
+      MaxRB(i) = 50;    
+      ReqRB(i,j) = UpLinkDataRate(j)/DataRate(i,j);
+      CeilofReqRB(i,j) = ceil(ReqRB(i,j));
+%     CeilofReqRBMatrix(count, :) = [x1(i) y1(i) x2(j) y2(j) CeilofReqRB(i,j)];
+    
     count = count + 1;    
     end
 end
 
-DistMatrix = sort(DistMatrix, 5);
-SINRdBMatrix = sort(SINRdBMatrix, 5);
-EfficiencyMatrix = sort(EfficiencyMatrix, 5);
-display(DistMatrix)
-display(SINRdBMatrix)
-display (EfficiencyMatrix)
+% DistMatrix = sort(DistMatrix, 5);
+% SINRdBMatrix = sort(SINRdBMatrix, 5);
+% EfficiencyMatrix = sort(EfficiencyMatrix, 5);
+% DataRateMatrix = sort(DataRateMatrix, 5);
+% CeilofReqRBMatrix = sort(CeilofReqRBMatrix, 5);
+% disp(DistMatrix)
+% disp(SINRdBMatrix)
+% disp(EfficiencyMatrix)
+% disp(DataRateMatrix)
+% disp(CeilofReqRBMatrix)
+
+disp(Distance)
+disp(SINRdB)
+disp(Efficiency)
+disp(DataRate)
+disp(ReqRB)
+disp(CeilofReqRB)
 
 figure;
 hold on;
